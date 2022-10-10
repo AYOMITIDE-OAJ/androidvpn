f204a82b2a852a8fa7752d975b07d4f9a7bfcaaa
Ayomitide_OAJ <ayomitide007@gmail.com>
2022-10-10 15:39:40 +0100

    neccessary utilities

---
+++ app/src/main/java/com/oajstudios/evpn/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.oajstudios.evpn.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.afollestad.materialdialogs.R;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/main/java/com/oajstudios/evpn/util/io/pem/PemObjectGenerator.java
@@ -0,0 +1,7 @@
+package com.oajstudios.evpn.util.io.pem;
+
+public interface PemObjectGenerator
+{
+    PemObject generate()
+            throws PemGenerationException;
+}
